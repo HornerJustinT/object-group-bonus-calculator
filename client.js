cf13664af408d9e851bc8e940662100655cfa37d
HornerJustinT <Horner.Justin.T@gmail.com>
2020-03-03 16:02:18 -0600

    Justin and Freida's Bonus Calculator

--- client.js
+++ client.js
@@ -1,5 +1,18 @@
-const employees = [
-  {
+$(document).ready(readyNow)
+function readyNow(){
+  console.log('JQ');
+  $('#clicker').on('click', showBonuses);
+}
+
+function showBonuses(){
+  let outputEl = $('#bonusList');
+  outputEl.empty();
+  for(element of bonusArray){
+    outputEl.append(`<li> <strong>Name:</strong> ${element.name} <strong>Bonus:</strong> ${element.totalBonus}</li>`)
+  }
+}
+
+const employees = [{
     name: 'Atticus',
     employeeNumber: '2405',
     annualSalary: '47000',
@@ -39,5 +52,55 @@ const employees = [
 
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
+let bonusArray = [];
+console.log(employees);
 
-console.log( employees );
+bonusCalculator = (peon) => {
+  
+  let tComp = peon.annualSalary*bonusCalculation(peon);
+  let tBonus = Math.trunc(tComp-peon.annualSalary);
+  let personBonus = {
+    name:peon.name,
+    bonusPercentage: bonusCalculation(peon),
+    totalCompensation: tComp,
+    totalBonus:tBonus
+  }
+  return personBonus;
+}
+bonusCalculation = (worker) =>{
+  let result = 1;
+  if(worker.reviewRating<=2){
+    result+= 0;
+  }
+  else if(worker.reviewRating==3){
+    result+= .04;
+  }
+  else if(worker.reviewRating==4){
+    result += .06;
+  }
+  else if(worker.reviewRating ==5){
+    result += .1;
+  }
+  if(worker.employeeNumber.length == 4){
+    result += .05;
+  }
+  if(worker.annualSalary>65000){
+    result -= .01
+  }
+  if(result>1.13){
+    result =1.13;
+  }
+  if(result<1){
+    result = 1;
+  }
+  return result
+}
+convertToBonusArray = (workerArray)=>{
+  let bonusArray=[];
+  for(worker of workerArray){
+    bonusArray.push(bonusCalculator(worker))
+  }
+  return bonusArray
+}
+bonusArray = convertToBonusArray(employees);
+console.log(convertToBonusArray(employees));
